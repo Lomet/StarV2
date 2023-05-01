b76a467412011517f2ee0d97ac714d4f48fbf21f
Stan Goldin <salty87@gmail.com>
2023-05-01 14:09:03 +0300

    add Ellipse

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿new Rectangle(5,8).Board.DrawBoard();
-
 new Star(15).Board.DrawBoard();
+new Ellipse(10, 10).Board.DrawBoard();
+
---
+++ Shapes/Ellipse.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using StarV2;
+
+public class Ellipse : IShape
+{
+    public Ellipse(int x, int y)
+    {
+        Middle = new Point(x, y);
+        Board = CreateEllipseBoard(Middle);
+    }
+    public Ellipse(Point middle)
+    {
+        Middle = middle;
+        Board = CreateEllipseBoard(middle);
+    }
+
+    public PixelBoard Board { get; }
+    private Point Middle { get; }
+
+    private PixelBoard CreateEllipseBoard(Point middle)
+    {
+        var Checks = new List<PixelCheck>
+        {
+            new PixelCheck(point => IsPointInEllipse(point))
+        };
+
+        return new PixelBoard(Checks, new Point(middle.X * 2 + 1, middle.Y * 2 + 1));
+    }
+
+    private bool IsPointInEllipse(Point point)
+    {
+        int dx = point.X - Middle.X;
+        int dy = point.Y - Middle.Y;
+        double distanceSquared = (double)dx * dx / (Middle.X * Middle.X) + (double)dy * dy / (Middle.Y * Middle.Y);
+
+        return distanceSquared <= 1;
+    }
+}
